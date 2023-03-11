cd094aecf1c057685e6a1a32557719e79390c951
limyshkalapala <limysh@dal.ca>
2023-03-11 15:38:48 -0400

    controller method implemented.

--- coder-gate/src/main/java/com/github/codergate/controllers/RepositoryController.java
+++ coder-gate/src/main/java/com/github/codergate/controllers/RepositoryController.java
@@ -5,12 +5,14 @@ import com.github.codergate.services.RepositoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -23,12 +25,13 @@ public class RepositoryController {
     private static final Logger LOGGER = LoggerFactory.getLogger(RepositoryController.class);
 
         @GetMapping("/getRepositories")
-        public ResponseEntity<String> fetchRepositories(@RequestBody String userId) {
-        LOGGER.debug("fetchRepositories : Entering the method");
-        if (userId != null && !userId.isEmpty()) {
-            List<RepositoriesAdded> repositoryFromUserId = repositoryService.getRepositoryFromUserId(Long.parseLong(userId));
-        }
-        LOGGER.debug("fetchRepositories : Exiting the method");
-        return ResponseEntity.ok(null);
+        public ResponseEntity<List<RepositoriesAdded>> fetchRepositories(@RequestBody String userId) {
+            LOGGER.debug("fetchRepositories : Entering the method");
+            List <RepositoriesAdded> repositoriesAdded = new ArrayList<>();
+            if (userId != null && !userId.isEmpty()) {
+                repositoriesAdded =repositoryService.getRepositoryFromUserId(Long.parseLong(userId));
+            }
+            LOGGER.debug("fetchRepositories : Exiting the method");
+            return ResponseEntity.ok(repositoriesAdded);
     }
 }
