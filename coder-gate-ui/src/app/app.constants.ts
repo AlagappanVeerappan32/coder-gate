f8832cd06eec586bcb9b8933ed6127b28b177962
Mayank Madan <my905874@dal.ca>
2023-04-12 12:15:22 -0300

    Update app.constants.ts

--- coder-gate-ui/src/app/app.constants.ts
+++ coder-gate-ui/src/app/app.constants.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = 'http://csci5308vm19.research.cs.dal.ca:8080';
-export const FRONTEND_URL = 'http://csci5308vm19.research.cs.dal.ca';
+export const BACKEND_URL = 'http://129.173.67.173:8080';
+export const FRONTEND_URL = 'http://129.173.67.173';
